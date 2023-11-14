a60c46e5acbb3ff168c92de5b14ab54a5b5b7c38
peter-berthelemy <peter.berthelemy@gmail.com>
2023-11-14 15:30:35 +0000

    Got rid of silly comments

--- downer.m
+++ downer.m
@@ -4,11 +4,7 @@ function out = downer(labelled)
 uni = unique(labelled);
 % downed = 1:size(uni);
 out = zeros(size(labelled));
-%Bish bash bosh
-%new comment by phoebe
-% new comment from neil, which is superior to previous comments
+
 for k = 1:size(uni)
     out(labelled == uni(k)) = k;
 end
-
-%1234
